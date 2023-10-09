35861343a01d6d80085b8f35f125bc5b776b8477
YU <111847207+YU000jp@users.noreply.github.com>
2023-10-09 17:40:37 +0900

    feat(Add): プラグイン設定の日本語対応(Japanese)

--- src/settings.ts
+++ src/settings.ts
@@ -1,31 +1,32 @@
 import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user';
+import { t } from 'logseq-l10n';
 
 export const settingsTemplate = (): SettingSchemaDesc[] => [
     {//子ブロックの数を求める
         key: "blockChildrenLength",
         type: "boolean",
-        title: "Enable the block children length",
+        title: t("Enable the block children length"),
         default: true,
         description: "default: true",
     },
     {//子ブロックのバイト数を求める
         key: "blockChildrenByteLength",
         type: "boolean",
-        title: "Enable the block children byte length",
+        title: t("Enable the block children byte length"),
         default: true,
         description: "default: true",
     },
     {//子ブロックの行数を求める
         key: "blockChildrenLineLength",
         type: "boolean",
-        title: "Enable the block children line length",
+        title: t("Enable the block children line length"),
         default: true,
         description: "default: true",
     },
     {//親ページ名を表示する
         key: "showParentPageName",
         type: "boolean",
-        title: "Enable the parent page name",
+        title: t("Enable the parent page name"),
         default: true,
         description: "default: true",
     }
